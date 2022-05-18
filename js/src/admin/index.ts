5572bc1e3f372edb7bd175f8ab7268e5b1ec8e94
Ian Morland <ian@morland.me>
2022-05-19 00:28:44 +0100

    Format js

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -2,7 +2,5 @@ import app from 'flarum/admin/app';
 import SettingsPage from './components/SettingsPage';
 
 app.initializers.add('blomstra/user-filter', () => {
-  app.extensionData
-    .for('blomstra-user-filter')
-    .registerPage(SettingsPage)
+  app.extensionData.for('blomstra-user-filter').registerPage(SettingsPage);
 });
